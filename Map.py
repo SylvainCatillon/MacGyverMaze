1433244a9a192c78cdb9d926b37740badcc7ed47
SylvainCatillon <sylvain.catillon@gmail.com>
2019-07-01 16:10:10 +0200

    First Map created, from a .txt file. (on new git branch: "TextAsMap")

--- Map.py
+++ Map.py
@@ -1,21 +1,31 @@
+
 class Map:
     """The map of the game"""
 
-    def __init__(self, squares, width, height):
-        self.squares = squares
-        self.width = width
-        self.height = height
-        self.squares_dict = {}
-        for i in range(1, height+1):
-            self.squares_dict[i] = [square for square in squares if square.y == i]
+    def __init__(self, map_name):
+        file_name = map_name + ".txt"
+        squares_list = []
+        with open(file_name, "r") as file:
+            string = file.read()
+            y_list = string.split("\n")
+            for i in range(len(y_list)-1,-1,-1):
+                squares_list.append([e for e in y_list[i]])
+        self.squares_list = squares_list
+        self.height = len(squares_list)
+        self.width = len(squares_list[0])
+
+    def get_square(self,x, y):
+        """Return the value of the square"""
+        return self.squares_list[y-1][x-1]
+
+    def is_wall(self, x, y):
+        """Return true if there is a wall on the square"""
+        return self.get_square(x, y) == "1"
 
 
-
-
-class Square:
-    """Square of map"""
-
-    def __init__(self, x, y, is_wall):
-        self.x = x
-        self.y = y
-        self.is_wall = is_wall
+map = Map("facile")
+print(map.get_square(3, 6))
+print(map.is_wall(3, 6))
+print(map.is_wall(2, 15))
+print(map.height)
+print(map.width)
