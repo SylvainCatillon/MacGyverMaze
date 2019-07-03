929890aa0070f85b379e8408a57c99a77878218b
SylvainCatillon <sylvain.catillon@gmail.com>
2019-07-03 12:12:09 +0200

    New classes: -Display -Game
    New file Main.py: launch the game

--- Display.py
+++ Display.py
@@ -2,19 +2,43 @@
 class Display:
     """The class in charge of the display"""
 
-    english = {
-        "welcome": "Welcome in the MazeGyver Game! Will you be able to get out of here?",
-        "congrats": "Congratulations, you reached the exit!!!" ,
-        "game_over": "How unfortunate, you're dead!",
-        "end": "Thank you for playing"
+    LANGUAGE_DICT = {
+        "english": {
+            "welcome": "Welcome in the MazeGyver Game! Will you be able to get out of here?",
+            "congrats": "Congratulations, you reached the exit!!!",
+            "game_over": "How unfortunate, you're dead!",
+            "end": "Thank you for playing"
+        }
     }
 
-def display_map(current_map):
-    display_string = ""
-    for y in range(current_map.height):
-        for x in range(current_map.width):
-            display_string += current_map.get_square((x, y))
-        display_string += "\n"
-    print(display_string)
+    def __init__(self, language, current_map):
+        self.language_dict = self.LANGUAGE_DICT[language]
+        self.current_map = current_map
+
+    @property
+    def welcome(self):
+        return self.language_dict["welcome"]
+
+    @property
+    def congrats(self):
+        return self.language_dict["congrats"]
+
+    @property
+    def game_over(self):
+        return self.language_dict["game_over"]
+
+    @property
+    def end(self):
+        return self.language_dict["end"]
+
+    def display_map(self):
+        display_string = ""
+        for y in range(self.current_map.height):
+            for x in range(self.current_map.width):
+                display_string += self.current_map.get_square((x, y))
+            display_string += "\n"
+        print(display_string)
+
+
 
 
--- Game.py
+++ Game.py
@@ -2,24 +2,29 @@ from Map import *
 from Display import *
 
 
-def init_game():
-    current_map = Map()
-    current_map.choose_map("facile")  # add input when several maps
-    return current_map
+class Game:
+    """The current game"""
 
+    def __init__(self):
+        self.map = Map()
+        self.map.choose_map("facile")  # add input
+        self.display = Display("english", self.map)  # add input
 
-def play(current_map):
-    while current_map.current_position != current_map.end:
-        display_map(current_map)
-        inp = ""
-        while inp not in current_map.get_available_directions():
-            inp = input("Choose a direction: {}".format(current_map.get_available_directions())).upper()
+    def choose_map(self):
+        self.map.choose_map("facile")  # add input
+
+    def play(self):
+        while self.map.current_position != self.map.end:
+            self.display.display_map()
+            inp = ""
+            while inp not in self.map.get_available_directions():
+                inp = input("Choose a direction: {}".format(self.map.get_available_directions())).upper()
+                print(inp)
+                if inp == "Q":
+                    break
             if inp == "Q":
-                print("Thank you for playing")
                 break
-        current_map.set_current_position(inp)
-    if current_map.current_position == current_map.end:
-        print("Congratulations, you reached the exit!!!")
-
-
-play(init_game())
+            self.map.set_current_position(inp)
+        if self.map.current_position == self.map.end:
+            print(self.display.congrats)
+        print(self.display.end)
